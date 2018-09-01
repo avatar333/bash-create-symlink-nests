3b95fceefbaec2e537d194a6b5e09a31745a2455
Kevinesan Pillay <kevin.pillay@is.co.za>
2018-09-01 11:39:50 +0200

    Initial basic configuration

---
+++ create-symlink-nests2.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+#title           :create-symlink-nests.sh
+#description     :A script to scrape mount points and create symlink nests
+#author		 :Kevin Pillay
+#date            :20180627
+#version         :1.0
+#usage		 :bash create-symlink-nests.sh
+#notes           :
+#bash_version    :GNU bash, version 4.4.19(1)-release (x86_64-redhat-linux-gnu)
+#==============================================================================
+
+# VAR / CONST
+PREFIX=/dockers
+PLEX_ROOT_PATH=${PREFIX}/plex
+PLEX_MOVIES=${PLEX_ROOT_PATH}/movies
+PLEX_TVSERIES=${PLEX_ROOT_PATH}/tvseries
+SRCPREFIX=/mnt
+
+# Change the separator value to cater for paths with spaces
+OIFS="$IFS"
+IFS=$'\n'
+
+# Check if the directory exists at the target and is a symlink
+function exists_as_symlink()
+{
+	ADIR=$1
+	TTYPE=$2
+	if [[ -L ${PLEX_ROOT_PATH}/${TTYPE}/$(basename $ADIR) ]]
+	then
+		echo 1
+	else
+		echo 0
+	fi
+}
+
+# Traverse mount points and create symlinks in target paths
+function scrape_mount_points()
+{
+	TYPE=$1
+	SRCPATH=$2
+	if [[ $TYPE = "movies" ]]
+	then
+		TARG=$PLEX_MOVIES
+	elif [[ $TYPE = "tvseries" ]]
+	then
+		TARG=$PLEX_TVSERIES
+	fi
+
+	printf "TARG : $TARG\n"
+
+	TARG=/dockers/plex/Kishan_movies
+	SRCPATH=/mnt/src_media01-1TB/media-kishan/Movies
+
+	for SRCDIR in $(ls -1d ${SRCPATH}/*)
+	do
+		if [[ $(exists_as_symlink $SRCDIR $TYPE) -eq 0 ]]
+		then
+			printf "ln -sf \"$SRCDIR\" \"$TARG/$(basename $SRCDIR)\"\n"
+			printf "ln -sf \"$SRCDIR\" \"$TARG/$(basename $SRCDIR)\"\n" | /bin/bash
+		else
+			printf "Symlink already exists: $(ls -d $TARG/$(basename $SRCDIR))\n"
+		fi
+	done
+}
+
+########
+# MAIN #
+########
+PLEX_MOVIES=$1
+SRC_PATH=$2
+
+scrape_mount_points movies /dockers/plex/Kishan_movies
+
+# Set separator back to default
+IFS="$OIFS"
