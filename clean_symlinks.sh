9eefcf823d1d2a35611a55856b8bb245531534fc
Kevinesan Pillay <kevin.pillay@is.co.za>
2018-09-07 17:07:42 +0200

    Added clean_symlinks script

---
+++ clean_symlinks.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#title           :clean_symlinks.sh
+#description     :A script to remove broken symlinks
+#author		 :Kevin Pillay
+#date            :20180907
+#version         :1.0
+#usage		 :bash clean_symlinks.sh
+#notes           :
+#bash_version    :GNU bash, version 4.4.19(1)-release (x86_64-redhat-linux-gnu)
+#==============================================================================
+
+function clean()
+{
+	MYPATH=$1
+	for SYM in $($MYPATH)
+	do
+		OUTPUT=$(readlink $MYPATH/$SYM)
+		if [[ -n $OUTPUT ]]
+		then
+			printf "Broken symlink: $(ls -l $MYPATH/$SYM)\n"
+			printf "rm -f $MYPATH/$SYM\n"
+		fi
+	done
+}
+
+clean /docker/plex/tvseries
